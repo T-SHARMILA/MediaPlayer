101046fd8adccc88842b1e4c6358558e97e7c88b
T-SHARMILA <39996798+T-SHARMILA@users.noreply.github.com>
2020-08-06 12:45:52 +0530

    Add files via upload

---
+++ module-info.java
@@ -0,0 +1,14 @@
+module main {
+
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.graphics;
+    requires javafx.media;
+    requires javafx.web;
+    requires javafx.swt;
+    requires javafx.swing;
+    requires javafx.base;
+
+    exports main;
+
+}
